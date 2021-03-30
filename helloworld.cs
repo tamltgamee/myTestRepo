61696aedab0796f315f01e96b490ee25cc107894
tamlt <tamlt@sandinh.net>
2021-03-30 17:17:19 +0700

    tao conflix

--- helloworld.cs
+++ helloworld.cs
@@ -4,7 +4,7 @@ namespace HelloWorld
     class Hello {         
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World!....make conflictttttt");
+            System.Console.WriteLine("Hello World!....make conflictttttt, toi sua file nay");
 			
 			System.Console.WriteLine("Hello World! on my branch!!!!!");
         }
